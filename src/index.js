1170fabe78ec94f3cda5b3c28efedea52e03b88b
ChuThuong <19h1120035@sv.ut.edu.vn>
2022-02-22 22:11:25 +0700

    fsgsfg

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
+const handlebars = require('express-handlebars');
+const hbs = handlebars.create({ extname: '.hbs' });
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
 app.use(morgan('combined'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
+// Template engine
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'))
 app.get('/', (req, res) => {
-    res.send('<h1>Thương Chu 1080</h1>');
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
